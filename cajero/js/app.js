61bed5b9bb3745803cefacf5c84df0937c192bd5
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2021-04-22 04:52:10 -0400

    cajero

--- cajero/js/app.js
+++ cajero/js/app.js
@@ -1,1 +1,89 @@
-//alert("cajero");
+var imagenes = [];
+imagenes["50"] = "img/billete50.png";
+imagenes["20"] = "img/billete20.png";
+imagenes["10"] = "img/billete10.png";
+
+class Billete
+{
+	constructor(v, c)
+	{
+		this.valor = v;
+		this.cantidad = c;
+		this.imagen = new Image();
+		this.imagen.src = imagenes[this.valor];
+	}
+}
+
+var caja = [];
+caja.push( new Billete(50, 3) );
+caja.push( new Billete(20, 5) );
+caja.push( new Billete(10, 5) );
+
+contar();
+
+var div = 0;
+var papeles = 0;
+
+var resultado = document.getElementById("result");
+var b = document.getElementById("retirar");
+b.addEventListener("click", entregarDinero);
+
+function entregarDinero()
+{
+	var dibujado = [];
+	var t = document.getElementById("dinero");
+	dinero = parseInt(t.value);
+	if (total >= dinero)
+	{
+		for(bi of caja)
+		{
+			if (dinero > 0)
+			{
+				div = Math.floor(dinero/bi.valor);
+				if (div>bi.cantidad)
+				{
+					papeles = bi.cantidad;
+				}
+				else
+				{
+					papeles = div;
+				}
+					bi.cantidad = bi.cantidad-papeles;
+				for (var i = 0; i < papeles; i++)
+				{
+					dibujado.push ( new Billete(bi.valor, 1) );
+				}
+				dinero -= (bi.valor * papeles);
+			}
+		}
+		if (dinero == 0)
+		{
+			resultado.innerHTML += "Se ha retirado: <br />";
+			for(var e of dibujado)
+			{		
+				resultado.innerHTML += "<img src=" + e.imagen.src + " />";
+			}
+			resultado.innerHTML += "<hr />";
+		contar();	
+		}
+		else
+		{
+			resultado.innerHTML += "No tengo los billetes para esa suma, intenta otro valor <hr />";
+		}
+
+	}
+	else
+	{
+		resultado.innerHTML += "Soy un cajero pobre y no tengo dinero :( <hr />";
+	}	
+}
+
+function contar()
+{
+	total = 0
+	for (var tot of caja)
+	{
+		total = total + tot.valor * tot.cantidad;
+	}
+	console.log(total);
+}
--- cajero/js/billete.js
+++ cajero/js/billete.js
@@ -4,14 +4,16 @@ class Billete{
 		this.cantidad = cantidad;
 	}
 }
-var textnode; 
 var atm = [];
-var entregado = [];
+atm.push(new Billete(100,5));
 atm.push(new Billete(50,10));
-atm.push(new Billete(20,20));
-atm.push(new Billete(10,20));
+atm.push(new Billete(20,30));
+atm.push(new Billete(10,10));
+atm.push(new Billete(5,5));
+contar();
 var div = 0;
 var papeles = 0;
+var entregado = [];
 var dinero = 0;
 var retirar = document.getElementById('retirar');
 var p = document.getElementById('result');
@@ -19,6 +21,7 @@ retirar.addEventListener('click', entregarDinero);
 function entregarDinero(){
 	var t = document.getElementById('dinero');
 	dinero = parseInt(t.value);
+	if(total >= dinero){
 	for (var bi of atm) {
 		if(dinero > 0){
 			div = Math.floor(dinero / bi.valor);
@@ -27,18 +30,36 @@ function entregarDinero(){
 			}else{
 				papeles = div;
 			}
-			entregado.push(new Billete(bi.valor, papeles));
+			for(var i = 0; i < papeles; i++){
+				entregado.push(new Billete(bi.valor, papeles));
+			}
 			dinero -= (bi.valor * papeles);
+			console.log(dinero);
+			console.log(papeles);
 		}
 	}
 	if(dinero > 0){
-		p.style.color = 'red';
-		p.innerHTML = 'sin efectivo sufiente';  
+			p.style.color = 'blue';
+				p.innerHTML += e.cantidad + ' billetes de $  ' + e.valor + '<br />';
+			
 	}else{
 		for (var e of entregado) {
-			p.style.color = 'blue';
-			p.innerHTML += e.cantidad + ' billetes de $  ' + e.valor + '<br />';
+			if(e.cantidad > 0){
+				p.style.color = 'blue';
+				p.innerHTML += e.cantidad + ' billetes de $  ' + e.valor + '<br />';
+			}
 		}
+		contar();
 	}
-	
+	}else{
+		p.style.color = 'red';
+		p.innerHTML = 'sin efectivo sufiente'; 
+	} 	
+}
+function contar(){
+	total = 0;
+	for (var tot of atm) {
+		total += (tot.valor * tot.cantidad);  
+	}
+	console.log(total);
 }
