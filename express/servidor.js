5d366ccf642f7ef48896f330a7104b96fbb78a85
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2021-04-23 10:45:39 -0600

    coreccion de errores del servidor

--- express/servidor.js
+++ express/servidor.js
@@ -1,16 +1,17 @@
-//para importar modulos es con require
-var hhtp = require('http');//modulo de nucleo
+// //para importar modulos es con require
+var http = require('http'); //modulo de nucleo
 //permite ejecutar js del lado del servidor
-var server = http/createServer(function(request, response){
-	response.writeHead(200,{'Content-Type':'text/html'});
-	response.write('<html><body>hola</body></html>');
-	response.end();
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('<html><body>hola</body></html>');
+    response.end();
 });
 
-server.listen(80);
+server.listen(5300);
 
-/*console.log("hello word");
-function sum(n1,n2){
-	return n1 + n2;
-}
-console.log('sum> ' + sum(3,6));*/
+// console.log("hello word");
+
+// function sum(n1, n2) {
+//     return n1 + n2;
+// }
+// console.log('sum> ' + sum(3, 6));
